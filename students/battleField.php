08d8ffbc8dbaa6ca1daab0952939027d23620480
bzouleka <bzouleka@gmail.com>
2018-02-27 12:55:22 +0100

    tp3, warrior battle

--- students/battleField.php
+++ students/battleField.php
@@ -6,6 +6,33 @@
 
   class BattleField extends BaseBattleField
   {
+    public static function createMyWarrior(){
+
+        $SuperZouzou = new MarvelWarrior('SuperZouzou', 100, 100, 100);
+        $SuperZouzou->setImageUrl('https://www.sbs.com.au/popasia/sites/sbs.com.au.popasia/files/styles/full/public/cauliflaheadergif.gif?itok=wbgi5U-z&mtime=1496207037');
+        $Kameameah = new Weapon('Kameameah', 100);
+        $Kameameah->setImageUrl('https://vignette.wikia.nocookie.net/dragonball/images/2/21/FK_4.jpg/revision/latest?cb=20170522025457');
+        $SuperZouzou->SetWeapon($Kameameah);
+
+        $SuperZouzou->saveNew();
 
 
+
+    }
+
+
+      public static function createOtherWarrior(){
+
+
+          $griffe = new Weapon('griffe', 50);
+          $griffe->setImageUrl('http://img.over-blog-kiwi.com/1/42/18/12/20180221/ob_7abc16_griffes.png');
+          $NoOne = new MarvelWarrior('NoOne');
+          $NoOne->setImageUrl('https://vignette.wikia.nocookie.net/marvel_dc/images/9/97/Justice_League_of_America_Vixen_Rebirth_Vol_1_1_Textless.jpg/revision/latest?cb=20161018002153');
+          $NoOne->SetWeapon($griffe);
+
+          $NoOne->saveNew();
+
+
+
+      }
   }
--- students/warrior.php
+++ students/warrior.php
@@ -3,9 +3,116 @@
 require_once __DIR__ . "/../base/localWarrior.php";
 
 
-$GLOBALS['warriorID'] = 'azertyuiop';
+$GLOBALS['warriorID'] = 'SuperZouzou';
 
 // Définissez vos class de combattants
 abstract class Warrior extends LocalWarrior {
+
+    public $id;
+    public $name;
+    public $speed;
+    public $life;
+    public $shield;
+    public $imageUrl;
+    public $weapon;
+
+    public function __construct($id, $speed=30, $life=100, $shield=20)
+    {
+
+        $this->id=$id;
+        $this->speed=$speed;
+        $this->life=$life;
+        $this->shield=$shield;
+
+
+
+    }
+    public function SetWeapon(Weapon $Weapon){
+
+        $this->weapon = $Weapon;
+
+    }
+    public function SetImageUrl($url){
+
+        $this->imageUrl=$url;
+    }
+
+
+
+};
+
+class StartrekWarrior extends Warrior {
+
+    public $mentalPower;
+
+    public function __construct($id, $speed=30, $life=100, $shield=20, $mentalPower=8)
+    {
+        parent::__construct($id, $speed, $life, $shield);
+
+        $this->mentalPower=$mentalPower;
+    }
+
+    public function power(){
+        return $this-> mentalPower;
+    }
+
+};
+
+
+class MarvelWarrior extends Warrior {
+
+    public $superPower;
+
+    public function __construct($id, $speed=30, $life=100, $shield=20, $superPower=100)
+    {
+        parent::__construct($id, $speed, $life, $shield);
+
+        $this->superPower=$superPower;
+
+
+    }
+
+    public function power(){
+        return $this-> superPower;
+    }
+
+};
+
+
+class PokemonWarrior extends Warrior {
+
+    public $level;
+
+
+
+    public function __construct($id, $speed=30, $life=100, $shield=20, $level=1)
+    {
+        parent::__construct($id, $speed, $life, $shield);
+
+        $this->level=$level;
+    }
+
+    public function power(){
+        return $this-> level;
+    }
+
+};
+
+class Weapon {
+
+    public $id;
+    public $strength;
+    public $imageUrl;
+
+    public function __construct($id, $strength)
+    {
+        $this->id=$id;
+        $this->strength=$strength;
+    }
+
+    public function setImageUrl($url){
+
+        $this->imageUrl=$url;
+    }
 
 };
